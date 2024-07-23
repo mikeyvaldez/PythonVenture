bc38254595bfa88baf1cb1c188b3be13f707a6b6
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-07-23 11:55:18 -0700

    first commit

---
+++ inputFunction/inputFunction.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+#------------------------------
+# Take user input to multiply two integers
+
+# num1 = int(input())
+# num2 = int(input())
+
+# print(num1 * num2)
+# ----------------------------
+
+
+# -------------------------------
+# Take user input and calculate the length of the string
+# You can use the len() function
+
+print(len(input()))
+
+
+# -------------------------------
+
+
