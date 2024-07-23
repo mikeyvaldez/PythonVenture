283e9801bc0905d12c79790cc9d0a8768fa7326b
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-07-23 12:29:57 -0700

    input function

--- inputFunction/inputFunction.py
+++ inputFunction/inputFunction.py
@@ -4,7 +4,7 @@
 # -----------------------------
 # Take user input to say hello to the user
 
-print("Hello " + input("What is your name? ") + "!")
+# print("Hello " + input("What is your name? ") + "!")
 # -----------------------------
 
 #------------------------------
@@ -22,8 +22,22 @@ print("Hello " + input("What is your name? ") + "!")
 # You can use the len() function
 
 # print(len(input()))
+# -------------------------------
 
+# -------------------------------
+# Using variables and the input function
 
+# There are two variables a and b
+a = input()
+b = input()
+
+# Uncommnet below to Create a third variable to help switch the values
+# c = a
+# a = b
+# b = c
+
+print("a: " + a)
+print("b: " + b)
 # -------------------------------
 
 
