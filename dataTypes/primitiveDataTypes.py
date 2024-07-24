58c2462dce4e512ce3f0b8ab0b9c9c07b9c40f5c
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-07-23 21:54:10 -0700

    data types

---
+++ dataTypes/primitiveDataTypes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+
+# Data Types
+
+# String
+print("Hello"[0])
+print("Hello"[1])
+print("Hello"[2])
+print("Hello"[3])
+print("Hello"[4])
+
+print("123" + "345")
+
+
+# Integer 
+print(123 + 345)
+
+# Float
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
