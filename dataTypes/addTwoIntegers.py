d9842f1bc882e689fa1bd506ee8310f7c7e77400
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-07-23 22:19:04 -0700

    add two digit number

---
+++ dataTypes/addTwoIntegers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+two_digit_num = input()
+
+first_digit = int(two_digit_num[0])
+second_digit = int(two_digit_num[1])
+
+# Add the two integers together
+two_digit_num = first_digit + second_digit
+
+print(two_digit_num)
