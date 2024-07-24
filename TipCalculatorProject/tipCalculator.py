dd560b3ed79986234b25e0e200a5538b1a832f27
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-07-24 13:36:19 -0700

    tip calculator

---
+++ TipCalculatorProject/tipCalculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+# If the bill has $150.00, split between 5 people, with 12% tip.
+
+# Each person should pay (150.00 / 5) * 1.12 = 33.6
+# Format the result to 2 decimal places = 33.60
+
