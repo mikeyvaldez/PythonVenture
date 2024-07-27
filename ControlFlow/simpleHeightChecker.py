7450fff64df3df9401372ea86a086e5351d81314
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-07-26 20:06:28 -0700

    simple height checker using if/else and conditional statements

---
+++ ControlFlow/simpleHeightChecker.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow toaller before you can ride.")
