cca060555973b0577dd273af85cb2f2c7c618cef
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-07-26 21:23:58 -0700

    working with nested if and elif statements

---
+++ ControlFlow/nestedIfAndElif.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# The following code uses nested if / else statements and conditionals
+# to ask the user their height and ask the user their age.
+# Depending on their answer the results will vary.
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if age < 12:
+        print("Please pay $5.")
+    elif age <= 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
