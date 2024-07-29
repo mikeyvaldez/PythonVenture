f03f9e38d36bd34406dec61d7a429dba8577ae92
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-07-29 13:44:14 -0700

    This is a version of the famous FizzBuzz interview question

---
+++ Loops/fizzBuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+target = 100
+for number in range(1, target + 1):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
