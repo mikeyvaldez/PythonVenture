f29b07f2e625d9b452b5847843c2010b880bad75
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-07-31 11:07:38 -0700

    defining and calling a function

---
+++ FunctionsAndKarel/DefiningAndCallingFunctions/defineAndCallFunctions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+# this is defining the function example -> def my_function():
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+# This is how you call a function
+my_function()
