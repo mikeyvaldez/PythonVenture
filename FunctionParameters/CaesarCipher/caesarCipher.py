d80fb5992ff771e374b162830ec55b9c174b6271
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-01 15:46:08 -0700

    caesar cipher final version

--- FunctionParameters/CaesarCipher/caesarCipher.py
+++ FunctionParameters/CaesarCipher/caesarCipher.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python3
 
+from art import logo
+
+# print cipher logo
+print(logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 # combine the encrypt() and decrypt() functions into a single function called caesar().
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
     if cipher_direction == "decode":
         shift_amount *= -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
     print(f"Here's the {direction}d result: {end_text}")
 
-# call the caesar() function, passing over the 'text',
-# 'shift' and 'direction' values.
-caesar(start_text = text, shift_amount = shift, cipher_direction = direction)
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+    # call the caesar() function, passing over the 'text',
+    # 'shift' and 'direction' values.
+    caesar(start_text = text, shift_amount = shift, cipher_direction = direction)
+
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if result == "no":
+        should_continue = False
+        print("Goodbye.")
 
 # Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(plain_text, shift_amount):
