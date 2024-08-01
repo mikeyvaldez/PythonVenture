e25ad36bf8f68fd0a38c6ad83658c3b146c27815
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-01 12:21:38 -0700

    lists and tuples in depth

---
+++ ListsAndTuplesDeepDive/listsIntro.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+computer_parts = ["computer", "monitor", "keyboard", "mouse", "power supply"]
+
+for part in computer_parts:
+    print(part)
+
+print()
+print(computer_parts[2])
+
+print(computer_parts[0:3]) # slicing a list to produce the first 3 items in a list
+print(computer_parts[-1])  # python medium that returns that last item in the list or sequence
