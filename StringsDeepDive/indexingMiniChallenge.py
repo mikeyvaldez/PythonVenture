2e7000837dfe4ba75062e0dd387bd6368159abb6
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-01 12:34:48 -0700

    string indexing

---
+++ StringsDeepDive/indexingMiniChallenge.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+
+# Create a program that uses string indexing
+# to print out 'we win' out of the word Norwegian Blue
+
+
+parrot = "Norweigian Blue"
+
+print(parrot[3])
+print(parrot[4])
+print(parrot[10])
+print(parrot[3])
+print(parrot[5])
+print(parrot[9])
