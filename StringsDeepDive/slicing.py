7a4a96673306ac935a2f095f53a866dd4fe0a1bb
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-01 12:56:31 -0700

    slicing strings

---
+++ StringsDeepDive/slicing.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+parrot = "Norwegian Blue"
+
+print(parrot[0:6])   # Norweg
+print(parrot[3:5])  # we
+print(parrot[0:9])  # Norwegian
+print(parrot[:9])   # Norwegian
+print(parrot[10:14])   # Blue (recommended)
+print(parrot[10:])    # Blue
+
+
+print(parrot[:6])     # Norweg
+print(parrot[6:])     # ian Blue
+print(parrot[:6] + parrot[6:])   # Norwegian Blue
+
+print(parrot[:])    # prints the whole string Norwegian Blue
+
+
+
+
+
+
