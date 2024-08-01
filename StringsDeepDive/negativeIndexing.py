ee26cd4e404af1145d17d46a0deac49244eecc35
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-01 12:45:01 -0700

    using negative indexing with strings

---
+++ StringsDeepDive/negativeIndexing.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Create a program that uses negative indexing
+# to print out 'we win' out of the word Norwegian Blue
+
+parrot = "Norweigian Blue"
+
+print(parrot[-12])
+print(parrot[-11])
+print(parrot[-5])
+print(parrot[-12])
+print(parrot[-8])
+print(parrot[-6])
+
+print(parrot[-5])
+
+print(parrot[2-14])
+print(parrot[3-14])
+print(parrot[9-14])
+print(parrot[2-14])
+print(parrot[6-14])
+print(parrot[8-14])
