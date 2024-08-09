a02e764f1ae4c9d87bc93c6aba776fd6a28b3501
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-08 21:31:26 -0700

    This is a Number guessing game program

---
+++ Scope/NumberGuessingGame/numberGuessingGame.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+difficulty_option = input("Type 'easy or 'hard': ")
+
