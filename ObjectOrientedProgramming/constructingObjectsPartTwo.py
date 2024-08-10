c7afd826f58a20e974928b8aaba30a57b97fb918
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-09 22:54:31 -0700

    using pretty tables

---
+++ ObjectOrientedProgramming/constructingObjectsPartTwo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+table.align = "l"
+
+print(table)
