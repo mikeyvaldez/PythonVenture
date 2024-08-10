9df87f189d118dc34d931c9d04abda9764d8f4c8
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-10 15:25:52 -0700

    edit

--- ObjectOrientedProgramming/creatingAClass.py
+++ ObjectOrientedProgramming/creatingAClass.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 
-class User:
-    pass
+# an Attribute is a variable that is associated with an object
+# The contructor initializes attributes..
 
-user_1 = User()
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+
+user_1 = User("001", "Mike")
+user_2 = User("002", "Bere")
+
+print(user_1.followers)
