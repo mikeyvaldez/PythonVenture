3de43ebdf270ab15e15e9a24eb5bc22ec679df60
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-12 17:51:04 -0700

    web scraping

--- WebScraping/ScrapingLiveSite/scrapingPartTwo.py
+++ WebScraping/ScrapingLiveSite/scrapingPartTwo.py
@@ -13,12 +13,13 @@
 
 
 
+# This program goes through the specific website and determines the highest score 
+# among the news titles of hacker news
 
-# extracts titles of the hyper links for the website
+import requests      # type: ignore
+from bs4 import BeautifulSoup     # type: ignore
 
 
-from bs4 import BeautifulSoup # type: ignore
-import requests # type: ignore
 
 response = requests.get("https://news.ycombinator.com/")
 
@@ -27,8 +28,33 @@ web_page = response.text
 soup = BeautifulSoup(web_page, "html.parser")
 
 titles = soup.select(".titleline a")
+links = soup.select(".titleline a")
+scores = soup.select(".score")
+
+title_list = []
+link_list = []
+score_list = []
 
 for title in titles:
     if "." in title.text:
         continue
-    print(title.text)
+    title_list.append(title.text)
+
+for link in links:
+    if "?" in link.get("href"):
+        continue
+    link_list.append(link.get("href"))
+
+for score in scores:
+    points = score.getText()
+    score_list.append(int(points.split()[0]))
+
+
+max_score = max(score_list)
+max_index = score_list.index(max_score)
+
+print(f"""
+{title_list[max_index]}\n
+{link_list[max_index]}\n
+{score_list[max_index]}\n
+""")
