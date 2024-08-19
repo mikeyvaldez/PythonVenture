df0e48557670f20b99b2acdfc95af673e3370678
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-18 22:22:26 -0700

    updated snake game

--- FilesDirectoriesAndPaths/ImprovedSnakeGame/main.py
+++ FilesDirectoriesAndPaths/ImprovedSnakeGame/main.py
@@ -16,7 +16,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
-Scoreboard = Scoreboard()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -35,19 +35,21 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        Scoreboard.increase_score()
+        scoreboard.increase_score()
 
     # Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        Scoreboard.game_over()
+        scoreboard.reset()        
+        snake.reset()
 
 
     # Detect collision with tail.
-    for segment in snake.segments[1:]:        
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            Scoreboard.game_over()
+    for segment in snake.segments[1:]:
+        if segment == snake.head:
+            pass 
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()           
 
 
 
