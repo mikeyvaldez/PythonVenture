91c0d9821fa92326d73c5c25010bdfebc9f55955
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-20 01:13:05 -0700

    Fifty states final

--- CSVDataAndPandasLibrary/FiftyStates/main.py
+++ CSVDataAndPandasLibrary/FiftyStates/main.py
@@ -18,28 +18,22 @@ guessed_states = []
 
 
 while len(guessed_states) < 50:
-
-    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 Guess the State",
-                                    prompt="What's another state's name?").title()
-    
-
-
-    if answer_state in all_states:     # If answer_state is one of the states in all the states of the 50_states.csv    
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?")
+    if answer_state == "Exit":   # type exit to stop the program and write to csv file
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
         guessed_states.append(answer_state)
-        t = turtle.Turtle()     # Create a turtle to write the name of the state at the state's x and y coordinate
+        t = turtle.Turtle()
         t.hideturtle()
         t.penup()
         state_data = data[data.state == answer_state]
-        t.goto(int(state_data.x.item()), int(state_data.y.item()))    
+        t.goto(state_data.x.item(), state_data.y.item())
         t.write(answer_state)
-        
 
-
-screen.exitonclick()
-
-
-
-
-
-
-# screen.exitonclick()
