dfddceb62991f060ba75a9e2776e497d67d33209
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-21 22:29:22 -0700

    dictionary comprehension

---
+++ DictionaryComprehension/usingDictionaryComprehension/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+# This is the key word structure for looping through a dictionary
+# {new_key:new_value for (key, value) in dictionar.items()}
+
+
+
+import random
+
+names = ["Henry", "Alex", "Lorenzo", "Beth", "Dave", "Ellouise", "Freddison"]
+
+student_score = {student:random.randint(1, 100) for student in names}
+
+passed_students = {student:score for (student, score) in student_score.items() if score >= 60}
+print(passed_students)
