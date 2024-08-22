9935dfb32f97d8c657d8f29664e44981415ca592
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-21 22:52:54 -0700

    dictionary comprehension exercise 2

---
+++ DictionaryComprehension/DictionaryCompExerciseTwo/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+
+weather_f = {day:temp * 9/5 + 32 for (day, temp) in weather_c.items()}
+
+print(weather_f)
