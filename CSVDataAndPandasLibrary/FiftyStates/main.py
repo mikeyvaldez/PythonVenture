7876372e58d667cfb9457cd0be7690a65abdd402
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-21 22:14:40 -0700

    edit to the fifty states game

--- CSVDataAndPandasLibrary/FiftyStates/main.py
+++ CSVDataAndPandasLibrary/FiftyStates/main.py
@@ -21,10 +21,8 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?")
     if answer_state == "Exit":   # type exit to stop the program and write to csv file
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
+
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
